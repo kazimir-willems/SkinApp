599e554cfba21addb208239bcd9b93c28a5e00ca
arthur <wsz@jxgi.com>
2017-10-18 11:38:52 +0100

    Basic Project

---
+++ app/src/main/java/skinapp/luca/com/LoginActivity.java
@@ -0,0 +1,26 @@
+package skinapp.luca.com;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_login)
+    void onClickBtnLogin() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+
+        startActivity(intent);
+    }
+}
