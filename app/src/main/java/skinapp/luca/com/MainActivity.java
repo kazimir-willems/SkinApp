0295b232ae6975501133a85ef809f718a6efdd6d
arthur <wsz@jxgi.com>
2017-10-19 11:37:44 +0100

    - UI building

--- app/src/main/java/skinapp/luca/com/MainActivity.java
+++ app/src/main/java/skinapp/luca/com/MainActivity.java
@@ -1,7 +1,11 @@
 package skinapp.luca.com;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_vip)
+    void onClickBtnVip() {
+        Intent intent = new Intent(MainActivity.this, VIPMainActivity.class);
+
+        startActivity(intent);
     }
 }
