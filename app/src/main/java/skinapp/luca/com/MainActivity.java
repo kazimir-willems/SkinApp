58d01fedde53584fab51cde933530e9bf2a9a528
kazimir-willems <kazimirwillems@outlook.com>
2017-10-20 07:42:55 +0100

    UI building

--- app/src/main/java/skinapp/luca/com/MainActivity.java
+++ app/src/main/java/skinapp/luca/com/MainActivity.java
@@ -1,8 +1,11 @@
 package skinapp.luca.com;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -22,5 +25,28 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, VIPMainActivity.class);
 
         startActivity(intent);
+    }
+
+    @OnClick(R.id.btn_second)
+    void onClickBtnSecond() {
+        LayoutInflater factory = LayoutInflater.from(this);
+        final View deleteDialogView = factory.inflate(R.layout.dialog_information, null);
+        final AlertDialog deleteDialog = new AlertDialog.Builder(this).create();
+        deleteDialog.setView(deleteDialogView);
+        deleteDialogView.findViewById(R.id.btn_yes).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //your business logic
+                deleteDialog.dismiss();
+            }
+        });
+        deleteDialogView.findViewById(R.id.btn_no).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteDialog.dismiss();
+            }
+        });
+
+        deleteDialog.show();
     }
 }
