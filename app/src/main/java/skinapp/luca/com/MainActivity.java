48e3a38d8ca7208ce008ddeef0c5c5e76b35f1df
arthur <wsz@jxgi.com>
2017-10-27 11:05:44 +0100

    UI Building

--- app/src/main/java/skinapp/luca/com/MainActivity.java
+++ app/src/main/java/skinapp/luca/com/MainActivity.java
@@ -1,16 +1,31 @@
 package skinapp.luca.com;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int DATE_DIALOG_ID = 0;
+
+    private int mYear;
+    private int mMonth;
+    private int mDay;
+
+    private EditText edtBirthday;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +33,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_DIALOG_ID:
+                return new DatePickerDialog(this,
+                        mDateSetListener,
+                        mYear, mMonth, mDay);
+        }
+        return null;
     }
 
     @OnClick(R.id.btn_vip)
@@ -31,6 +57,8 @@ public class MainActivity extends AppCompatActivity {
     void onClickBtnSecond() {
         LayoutInflater factory = LayoutInflater.from(this);
         final View informDialogView = factory.inflate(R.layout.dialog_information, null);
+        edtBirthday = (EditText) informDialogView.findViewById(R.id.edt_birthday);
+
         final AlertDialog informDialog = new AlertDialog.Builder(this).create();
         informDialog.setView(informDialogView);
         informDialogView.findViewById(R.id.btn_yes).setOnClickListener(new View.OnClickListener() {
@@ -50,7 +78,46 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        edtBirthday.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    showDialog(DATE_DIALOG_ID);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        // display the current date
+        updateDisplay();
+
         informDialog.show();
+    }
+
+    private DatePickerDialog.OnDateSetListener mDateSetListener =
+            new DatePickerDialog.OnDateSetListener() {
+                public void onDateSet(DatePicker view, int year,
+                                      int monthOfYear, int dayOfMonth) {
+                    mYear = year;
+                    mMonth = monthOfYear;
+                    mDay = dayOfMonth;
+                    updateDisplay();
+                }
+            };
+
+    private void updateDisplay() {
+        this.edtBirthday.setText(
+                new StringBuilder()
+                        // Month is 0 based so add 1
+                        .append(mYear).append("/")
+                        .append(mMonth + 1).append("/")
+                        .append(mDay));
     }
 
     @OnClick(R.id.btn_third)
