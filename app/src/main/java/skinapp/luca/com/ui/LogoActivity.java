20b0a208477816211a75db25fa9be7799dab6b65
kazimir-willems <kazimirwillems@outlook.com>
2017-11-03 03:04:25 +0100

    - UI buiding

---
+++ app/src/main/java/skinapp/luca/com/ui/LogoActivity.java
@@ -0,0 +1,15 @@
+package skinapp.luca.com.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import skinapp.luca.com.R;
+
+public class LogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+    }
+}
